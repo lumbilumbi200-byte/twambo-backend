780b207859464076984cc2fa37614a28ab4c6461
MansLikeNight <evabloo2342@gmail.com>
2026-06-28 05:03:42 +0200

    Enable PostGIS extension during build

--- build.sh
+++ build.sh
@@ -3,5 +3,13 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+python -c "
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+django.setup()
+from django.db import connection
+with connection.cursor() as cursor:
+    cursor.execute('CREATE EXTENSION IF NOT EXISTS postgis')
+"
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
